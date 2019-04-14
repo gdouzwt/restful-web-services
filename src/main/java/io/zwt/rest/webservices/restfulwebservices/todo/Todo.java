2c87039e5bbd4b5665624439b390aeff8afe9a2b
招文桃 <gdouzwt@gmail.com>
2019-04-15 03:31:23 +0800

    CRUD completed

--- src/main/java/io/zwt/rest/webservices/restfulwebservices/todo/Todo.java
+++ src/main/java/io/zwt/rest/webservices/restfulwebservices/todo/Todo.java
@@ -1,6 +1,7 @@
 package io.zwt.rest.webservices.restfulwebservices.todo;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Todo {
 
@@ -9,7 +10,11 @@ public class Todo {
 	private String description;
 	private Date targetDate;
 	private boolean isDone;
-	
+
+	protected Todo() {
+
+	}
+
 	public Todo(long id, String username, String description, Date targetDate, boolean isDone) {
 		super();
 		this.id = id;
@@ -49,7 +54,17 @@ public class Todo {
 	public void setDone(boolean isDone) {
 		this.isDone = isDone;
 	}
-	
-	
-	
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Todo todo = (Todo) o;
+		return id == todo.id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 }
--- src/main/java/io/zwt/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
+++ src/main/java/io/zwt/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
@@ -9,16 +9,46 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TodoHardcodedService {
-	private static List<Todo> todos = new ArrayList<Todo>();
-	private static int idCounter = 0;
-	
-	static {
-		todos.add(new Todo(++idCounter, "tao", "Learn to Love", new Date(), false));
-		todos.add(new Todo(++idCounter, "tao", "To love Wing", new Date(), false));
-		todos.add(new Todo(++idCounter, "tao", "To love you more", new Date(), false));
-	}
-	
-	public List<Todo> findAll() {
-		return todos;
-	}
+    private static List<Todo> todos = new ArrayList<Todo>();
+    private static int idCounter = 0;
+
+    static {
+        todos.add(new Todo(++idCounter, "tao", "Learn to Code", new Date(), false));
+        todos.add(new Todo(++idCounter, "tao", "To Teach Code", new Date(), false));
+        todos.add(new Todo(++idCounter, "tao", "Get Project Done", new Date(), false));
+    }
+
+    public List<Todo> findAll() {
+        return todos;
+    }
+
+    public Todo save(Todo todo) {
+        if (todo.getId() == -1 || todo.getId() == 0) {
+            todo.setId(++idCounter);
+            todos.add(todo);
+        } else {
+            deleteById(todo.getId());
+            todos.add(todo);
+        }
+        return todo;
+    }
+
+
+    public Todo deleteById(long id) {
+        Todo todo = findById(id);
+        if (todo == null) return null;
+        if (todos.remove(todo)) {
+            return todo;
+        }
+        return null;
+    }
+
+    public Todo findById(long id) {
+        for (Todo todo : todos) {
+            if (todo.getId() == id) {
+                return todo;
+            }
+        }
+        return null;
+    }
 }
